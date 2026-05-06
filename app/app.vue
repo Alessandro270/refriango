fffdd1bea9478ef50916ed6da0dc97127cc07076
Alessandro270 <propriedade0@gmail.com>
2026-05-06 16:15:29 +0100

    first commit

---
+++ app/app.vue
@@ -0,0 +1,24 @@
+<script setup>
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'pt'
+  }
+})
+</script>
+
+<template>
+  <UApp>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+
+
+  </UApp>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    HELLO WORLD!
+    <UButton> Hello </UButton>
+  </div>
+</template>
