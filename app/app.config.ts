5a70d5ef769f657e7e2b497931fb16cc6ecf07fb
Alessandro270 <propriedade0@gmail.com>
2026-05-26 17:19:34 +0100

    Adding grid layout

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,44 +1,44 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: "amber",
-      secondary: "red",
-      neutral: "zinc",
+      primary: 'amber',
+      secondary: 'red',
+      neutral: 'zinc'
     },
     table: {
       slots: {
-        base: "bg-white text-default rounded-md",
-        th: "text-ui-text capitalize",
-        thead: "text-grays-900 bg-zinc-50",
-        td: "text-grays-600",
-        tr: "border-0",
-        separator: "bg-transparent",
-      },
+        base: 'bg-white text-default rounded-md',
+        th: 'text-ui-text capitalize',
+        thead: 'text-grays-900 bg-zinc-50',
+        td: 'text-grays-600',
+        tr: 'border-0',
+        separator: 'bg-transparent'
+      }
     },
     badge: {
       slots: {
-        base: "font-bold uppercase text-ui-text bg-zinc-200 rounded-sm",
-        label: "truncate",
+        base: 'font-bold uppercase text-ui-text bg-zinc-200 rounded-sm',
+        label: 'truncate'
       },
       defaultVariants: {
-        color: "neutral",
-        variant: "soft",
-        size: "sm",
-      },
+        color: 'neutral',
+        variant: 'soft',
+        size: 'sm'
+      }
     },
     button: {
       defaultVariants: {
-        color: "neutral",
-        variant: "soft",
-        size: "md",
-      },
+        color: 'neutral',
+        variant: 'soft',
+        size: 'md'
+      }
     },
     input: {
       variants: {
         variant: {
-          outline: "text-zinc-600 bg-transparent ring transition ring-zinc-300",
-        },
-      },
-    },
-  },
-});
+          outline: 'text-zinc-600 bg-transparent ring transition ring-zinc-300'
+        }
+      }
+    }
+  }
+})
--- app/pages/config.vue
+++ app/pages/config.vue
@@ -1,3 +1,71 @@
+<script setup lang="ts">
+const uiStyle = {
+  label: 'text-ui-text',
+  root: 'h-full',
+  container: 'flex-1 flex flex-col mt-1'
+}
+const inputStyle = {
+  label: 'text-ui-text',
+  root: 'h-full',
+  container: 'flex-1 flex flex-col mt-1'
+}
+
+const fileUploadStyle = { base: 'h-full' }
+</script>
+
 <template>
-  <div>CONFIGURACOES</div>
+  <div class="space-y-4">
+    <UiH1>CONFIGURACOES</UiH1>
+    <UForm
+      class="bg-white rounded-md px-6 py-6 gap-4 grid grid-cols-12 auto-rows-fr"
+    >
+      <UFormField
+        :ui="uiStyle"
+        label="Primeiro Nome"
+        class="col-start-3 col-span-5"
+      >
+        <UInput :ui="inputStyle" />
+      </UFormField>
+      <UFormField :ui="uiStyle" label="Ultimo Nome" class="col-span-5">
+        <UInput :ui="inputStyle" />
+      </UFormField>
+      <UFileUpload
+        class="col-start-1 col-span-2 row-start-1 row-span-2"
+        label="Foto de perfil"
+        :ui="fileUploadStyle"
+      />
+
+      <UFormField :ui="uiStyle" label="Senha" class="row-start-2 col-span-5">
+        <UInput :ui="inputStyle" />
+      </UFormField>
+      <UFormField
+        :ui="uiStyle"
+        label="Confirmar Senha"
+        class="row-start-2 col-span-5"
+      >
+        <UInput :ui="inputStyle" />
+      </UFormField>
+      <UFormField
+        :ui="uiStyle"
+        label="Numero De Telefone"
+        class="row-start-3 col-start-3 col-span-10"
+      >
+        <UInput :ui="inputStyle" />
+      </UFormField>
+      <UFormField
+        :ui="uiStyle"
+        label="Data De Nascimento"
+        class="row-start-4 col-start-3 col-span-10"
+      >
+        <UInput :ui="inputStyle" />
+      </UFormField>
+      <UFormField
+        :ui="uiStyle"
+        label="Numero De BI"
+        class="row-start-5 col-start-3 col-span-10"
+      >
+        <UInput :ui="inputStyle" />
+      </UFormField>
+    </UForm>
+  </div>
 </template>
